abc4eae59530a7c7a6b5c87ba19cac2cb9e7cb85
Frostrion <sastoinev@gmail.com>
2020-12-26 13:21:03 +0200

    first commit

---
+++ newapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace newapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
